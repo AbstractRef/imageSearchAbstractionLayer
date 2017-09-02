d73267d8c2e790b180a11f086ffd4a8a80418e5b
System <none>
2017-09-02 09:23:40 +0000

    Checkpoint

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Backend Challenges boilerplate - package.json
+# FreeCodeCamp :: Image Search Abstraction Layer  
+
+**User Story:** I can get the image URLs, alt text and page urls for a set of images relating to a given search string.  
+  
+**User Story:** I can paginate through the responses by adding a ?offset=2 parameter to the URL.  
+  
+**User Story:** I can get a list of the most recently submitted search strings.  
+```http
+https://cryptic-ridge-9197.herokuapp.com/api/imagesearch/lolcats%20funny?offset=10
--- server.js
+++ server.js
@@ -36,27 +36,30 @@ app.route('/_api/package.json')
   });
 
 app.route('/test').get(function(req, res, next) {
-client.search('Steve Angello')
+client.search('walking dead')
 	.then(images => {
-  console.log("returned from promise")
-		/*
-		[{
-			"url": "http://steveangello.com/boss.jpg",
-			"type": "image/jpeg",
-			"width": 1024,
-			"height": 768,
-			"size": 102451,
-			"thumbnail": {
-				"url": "http://steveangello.com/thumbnail.jpg",
-				"width": 512,
-				"height": 512
-			}
-		}]
-		 */
-res.send(JSON.stringify(images));
-	}).catch(err => {
-  res.send(err);
-})
+/*
+		[
+    {
+      type: "image/jpeg",
+      width: 3600,
+      height: 2403,
+      size: 1047219,
+      url: "http://static2.businessinsider.com/image/56bb5129dd08958c148b470a-3600-2403/the-walking-dead-zombies.jpg",
+      thumbnail: {
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRpurq3kfA3kvVA62f595fiaaPoZro8bKfxGDug__6X5Q0iqRtQLpR8pqf",
+        width: 150,
+        height: 100
+      },
+      description: "The Walking Dead' used zombie dentures with blood in mid-season ...",
+      parentPage: "http://www.businessinsider.com/walking-dead-zombie-dentures-mid-season-premiere-2016-2"
+    }
+    ]
+*/
+    res.send(JSON.stringify(images));
+  }).catch(err => {
+    res.send(err);
+  })
 });
 
 
