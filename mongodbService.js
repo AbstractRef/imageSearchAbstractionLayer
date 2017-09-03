7733c061ed48bb7d9d15b53fb6306949f8f3bcc0
System <none>
2017-09-03 09:35:53 +0000

    Checkpoint

---
+++ mongodbService.js
@@ -0,0 +1,64 @@
+var mongodb = require('mongodb');
+var MONGODB_URI = 'mongodb://' + process.env.USER + ':' + process.env.PASS + '@' + process.env.HOST + ':' + process.env.DB_PORT + '/' + process.env.DB;
+var collection = process.env.COLLECTION;
+
+var state = {
+    db: null,
+    record: null
+}
+
+function connect() {
+    if (state.db)
+        return;
+
+    mongodb.MongoClient.connect(MONGODB_URI)
+        .then(function(db) {
+            state.db = db;
+            console.log('Connection established to', MONGODB_URI);
+            return;
+        })
+        .catch(function(err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+            throw err;
+        });
+}
+
+function close() {
+    if (state.db) {
+        state.db.close(function(err, result) {
+            state.db = null
+            state.mode = null
+        });
+    }
+}
+
+function getDb() {
+    if (!state.db) {
+        connect();
+    }
+    return state.db;
+}
+
+function get() {
+    return state.record;
+}
+
+function storeSearchDetail(searchTerm) {
+
+}
+
+function getRecentSearches() {
+    return {
+        "search": "test",
+        "date": "1/1/17"
+    };
+}
+
+var mongoSeachDatastore = {
+    storeSearchDetail: storeSearchDetail,
+    getRecentSearches: getRecentSearches
+};
+
+module.exports = {
+    mongoSeachDatastore: mongoSeachDatastore
+};
